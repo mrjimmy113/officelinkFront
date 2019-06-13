3a2115cfb3848d8e4f4a9264cbb2f3e5bd3846d5
Duong <phduongse@gmail.com>
2019-06-13 20:37:01 +0700

    13-6

--- src/app/department-save/department-save.component.ts
+++ src/app/department-save/department-save.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, Output } from '@angular/core';
 import { Department } from '../model/department';
 import { ModalService } from '../service/modal.service';
 import { DepartmentService } from '../service/department.service';
+import { Location } from '../model/location';
 
 @Component({
   selector: 'app-department-save',
@@ -12,12 +13,14 @@ export class DepartmentSaveComponent implements OnInit {
   @Input() inputs;
   @Output() outputs;
   department: Department;
+  notSelectedLocations: Array<Location>;
   requestStatus: Number;
   isEdit = false;
 
   constructor(private modalSer: ModalService, private ser: DepartmentService) { }
 
   ngOnInit() {
+    this.notSelectedLocations = this.generateLocation(6);
     this.init();
   }
 
@@ -84,11 +87,43 @@ export class DepartmentSaveComponent implements OnInit {
     this.ser.getDepartmentWithTeams(depId).subscribe(
       result => {
         this.department = result;
-        console.log(this.department);
+        this.department.locations = this.generateLocation(1);
       },
       error => {
         alert("Something went wrong. Try again later.");
         this.closeModal();
       });
+  }
+
+  generateLocation(startId) {
+    // test location
+    let locations = new Array<Location>();
+    var i = startId;
+    for (i = 1; i <= 5; i++) {
+      var loc: Location = {
+        id: i,
+        address: "Address" + i,
+        city: "City" + i,
+        county: "County" + i,
+        dateCreated: null,
+        dateModified: null,
+        isDeleted: false,
+      }
+      locations.push(loc);
+    }
+    return locations;
+    // end test
+  }
+
+  addLocation(location) {
+
+  }
+
+  removeLocation(locId) {
+    var loc = this.department.locations.find(function (el) {
+      return el.id == locId;
+    })
+    var index = this.department.locations.indexOf(loc);
+    this.department.locations.splice(index, 1);
   }
 }
