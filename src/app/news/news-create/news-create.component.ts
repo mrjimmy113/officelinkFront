dbce726ab2f00b434f37c1883fd43632f491d9e6
CUONGTP\Thai Phu Cuong <icuong2302@gmail.com>
2019-06-17 04:37:33 +0700

    fix - init

--- src/app/news/news-create/news-create.component.ts
+++ src/app/news/news-create/news-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input, Output } from '@angular/core';
+import { News } from 'src/app/model/news';
+import { ModalService } from 'src/app/service/modal.service';
+import { NewsService } from 'src/app/service/news.service';
 
 @Component({
   selector: 'app-news-create',
@@ -7,9 +10,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsCreateComponent implements OnInit {
 
-  constructor() { }
+  @Input() inputs;
+  @Output() outputs;
+  news: News;
+  requestStatus: Number;
+  isEdit = false;
+
+
+  constructor(private modalService: ModalService, private service: NewsService) { }
 
   ngOnInit() {
+    this.init();
   }
 
+  init() {
+    if (this.inputs.length == 0) {
+      this.news = new News();
+    } else {
+      this.news = this.inputs;
+      this.isEdit = true;
+    }
+    this.requestStatus = 0;
+  }
+
+  close() {
+    this.modalService.destroy();
+  }
+
+  add() {
+    this.service.create(this.news).subscribe(result => {
+      this.requestStatus = result;
+      if (this.requestStatus == 201) {
+        alert("Create Successful");
+        this.close();
+      }
+      this.outputs();
+    },
+      error => {
+        if (error.status == 409) {
+          alert("Title cannot be duplicated");
+        } else if (error.status = 404) {
+          alert("Bad request");
+        }
+        this.close();
+        this.outputs();
+      }
+    );
+  }
+
+  update() {
+    this.service.update(this.news).subscribe(result => {
+      this.requestStatus = result;
+      if (this.requestStatus == 200) {
+        alert("Update Successful");
+        this.close();
+      }
+      this.outputs();
+    });
+  }
+
+  save() {
+    this.requestStatus = 1;
+    if (this.isEdit) this.update();
+    else this.add();
+  }
 }
--- src/app/news/news-main/news-main.component.ts
+++ src/app/news/news-main/news-main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/service/modal.service';
+import { NewsService } from 'src/app/service/news.service';
+import { NewsCreateComponent } from '../news-create/news-create.component';
 
 @Component({
   selector: 'app-news-main',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsMainComponent implements OnInit {
 
-  constructor() { }
+  itemList;
+  currentPage = 1;
+  maxPage;
+  searchTerm = "";
+  requestStatus:Number;
+
+  constructor(private modalService: ModalService, private service: NewsService) { }
 
   ngOnInit() {
+    this.search("");
   }
 
+  search(value) {
+    this.service.search(value).subscribe(result => {
+      this.maxPage = result.maxPage;
+      this.itemList = result.objList;
+    })
+  }
+
+  filter() {
+    let newSearchTerm = this.searchTerm;
+    setTimeout(() => {
+      if (newSearchTerm == this.searchTerm) {
+        this.search(this.searchTerm);
+      }
+    }, 300);
+  }
+
+  create() {
+    this.modalService.init(NewsCreateComponent,[],() => this.search(""));
+  }
+
+  edit(item) {
+    this.modalService.init(NewsCreateComponent,item,() => this.search(""));
+  }
+
+  delete(id) {
+    this.service.delete(id).subscribe(result => {
+      this.requestStatus = result;
+      if (this.requestStatus == 200){
+        alert("success");
+        this.search("");
+      }
+    });
+  }
 }
+
